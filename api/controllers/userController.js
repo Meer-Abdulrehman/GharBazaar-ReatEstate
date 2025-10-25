cca616206b00a1bb578e8fc45df5dc7b5cb98ef3
Abdul Rehman <your-abdulrehmantech.me@gmail.com>
2025-10-25 07:04:07 -0700

    user model and user controller update

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,79 +1,122 @@
-import bcryptjs from 'bcryptjs';
-import User from '../models/userModels.js';
-import { errorHandler } from '../utils/error.js';
-import Listing from '../models/listingModel.js';
+import bcryptjs from "bcryptjs";
+import User from "../models/userModels.js";
+import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listingModel.js";
+import jwt from "jsonwebtoken";
 
-
+// Test route
 export const test = (req, res) => {
-  res.json({
-    message: 'Api route is working!',
-  });
+  res.json({ message: "Api route is working!" });
 };
 
+// Update user
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only update your own account!'));
+    return next(errorHandler(401, "You can only update your own account!"));
+
   try {
-    if (req.body.password) {
-      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    const updates = { ...req.body };
+
+    // Hash password if provided
+    if (updates.password) {
+      updates.password = bcryptjs.hashSync(updates.password, 10);
     }
 
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
-      {
-        $set: {
-          username: req.body.username,
-          email: req.body.email,
-          password: req.body.password,
-          avatar: req.body.avatar,
-        },
-      },
+      { $set: updates },
       { new: true }
     );
 
     const { password, ...rest } = updatedUser._doc;
-
     res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
 };
 
+// Delete user
 export const deleteUser = async (req, res, next) => {
   if (req.user.id !== req.params.id)
-    return next(errorHandler(401, 'You can only delete your own account!'));
+    return next(errorHandler(401, "You can only delete your own account!"));
+
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json('User has been deleted!');
+    res.clearCookie("access_token");
+    res.status(200).json({ success: true, message: "User has been deleted!" });
   } catch (error) {
     next(error);
   }
 };
 
+// Get user listings
 export const getUserListings = async (req, res, next) => {
-  if (req.user.id === req.params.id) {
-    try {
-      const listings = await Listing.find({ userRef: req.params.id });
-      res.status(200).json(listings);
-    } catch (error) {
-      next(error);
-    }
-  } else {
-    return next(errorHandler(401, 'You can only view your own listings!'));
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only view your own listings!"));
+
+  try {
+    const listings = await Listing.find({ userRef: req.params.id });
+    res.status(200).json(listings);
+  } catch (error) {
+    next(error);
   }
 };
 
+// Get single user
 export const getUser = async (req, res, next) => {
   try {
-    
     const user = await User.findById(req.params.id);
-  
-    if (!user) return next(errorHandler(404, 'User not found!'));
-  
-    const { password: pass, ...rest } = user._doc;
-  
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const { password, ...rest } = user._doc;
     res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Signup or Google OAuth
+export const registerUser = async (req, res, next) => {
+  try {
+    const { name, email, password, avatar } = req.body;
+
+    if (!name || !email) {
+      return next(errorHandler(400, "Name and email are required!"));
+    }
+
+    let user = await User.findOne({ email });
+    if (user) return next(errorHandler(400, "User already exists!"));
+
+    const hashedPassword = password ? bcryptjs.hashSync(password, 10) : undefined;
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      avatar,
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("access_token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+    });
+
+    res.status(201).json({
+      success: true,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar,
+      },
+    });
   } catch (error) {
     next(error);
   }
--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,26 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  avatar:{
+const userSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+      required: true,
     },
-}, { timestamps: true });  
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: false, // Google login ke liye optional
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
-
 export default User;
